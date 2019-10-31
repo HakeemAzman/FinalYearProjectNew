349b21b57370ad52366c1da50e402ed2b3af44e6
Dania Mohd <yuukidania@gmail.com>
2019-10-31 10:53:13 +0800

    Puzzle 2 WIP

---
+++ Assets/_Scripts/Scripted Events/PulleySystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulleySystem : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.contactCount == 1)
+        {
+            //open gate halfway
+        }
+        else if(other.contactCount > 1)
+        {
+            //open gate fully
+        }
+    }
+}

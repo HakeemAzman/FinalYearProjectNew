23f4e6aeb287ce33c0ffab757ec21e1aab0f5849
Hakeem Azman <nurhakeemazman@gmail.com>
2019-10-31 21:29:53 +0800

    Intro Cutscene Polished and ready for sounds!

---
+++ Assets/_Scripts/Misc_/Intro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject introCutscene, introPlane, playerChar, playerCharUI;
+    float timer = 24.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            introCutscene.SetActive(false);
+            introPlane.SetActive(false);
+            playerChar.SetActive(true);
+            playerCharUI.SetActive(true);
+        }
+
+        if (timer <= -3f) Destroy(this.gameObject);
+        print(timer);
+    }
+}

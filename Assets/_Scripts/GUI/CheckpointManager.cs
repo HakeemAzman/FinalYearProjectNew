8e941c4e4304ce2118c8c18e43a08a739e02e133
Dania Mohd <yuukidania@gmail.com>
2019-10-31 16:48:08 +0800

    Checkpoint WIP

---
+++ Assets/_Scripts/GUI/CheckpointManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckpointManager : MonoBehaviour
+{
+    private static int m_checkpointNumber;
+
+    private Transform m_targetPlayer;
+    float m_playerHealth;
+    
+    void Start()
+    {
+        m_playerHealth = 0;
+
+        m_targetPlayer = PlayerManager.instance.player.transform;
+        m_playerHealth = m_targetPlayer.GetComponent<PlayerHealth>().player_Health;
+    }
+    
+    void Update()
+    {
+        if(m_playerHealth == 0)
+        {
+            //restart
+        }
+    }
+}

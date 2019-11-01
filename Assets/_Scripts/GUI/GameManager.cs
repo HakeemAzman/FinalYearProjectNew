418b48a5668ee71327efd90b002a01cfd1759eb8
Dania Mohd <yuukidania@gmail.com>
2019-11-01 17:46:26 +0800

    Pause Screen added

--- Assets/_Scripts/GUI/GameManager.cs
+++ Assets/_Scripts/GUI/GameManager.cs
@@ -8,8 +8,43 @@ public class GameManager : MonoBehaviour
 
     public Transform lastCheckpoint;
 
+    [SerializeField] private GameObject pausePanel;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            else if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
     }
 }

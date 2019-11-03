3c0dec046b4dc4ca87cb71a977826fad982e2fba
Nick Lee <44768335+SmallRunt@users.noreply.github.com>
2019-11-03 19:26:58 +0800

    Puzzle 2 Implemented into Main Level

--- Assets/_Scripts/Puzzle/PuzzleOneManager.cs
+++ Assets/_Scripts/Puzzle/PuzzleOneManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PuzzleOneManager : MonoBehaviour
 {
-    [SerializeField] GameObject cubeA, cubeB, gate, robotCage;
+    [SerializeField] GameObject cubeA, cubeB, gate, robotCage, cageObstacle,GateAnimator;
 
     [SerializeField] AudioClip gateOpeningSound, heavyGateOpeningSound;
     AudioSource audioS;
@@ -35,6 +35,17 @@ public class PuzzleOneManager : MonoBehaviour
         {
             audioS.PlayOneShot(heavyGateOpeningSound, 0.3f);
         }
+
+        if ((other.gameObject.name == "ObstacleChain") && Input.GetButtonDown("Stay"))
+        {
+            StartCoroutine("LiftDown");
+        }
+
+        if(other.gameObject.name == "PressurePlatePuzzle2")
+        {
+          GateAnimator.gameObject.GetComponent<Animator>().Play("GateOpenPuzzle2"); 
+        }
+    
     }
 
     private void OnTriggerStay(Collider other)
@@ -58,5 +69,17 @@ public class PuzzleOneManager : MonoBehaviour
         {
             robotCage.GetComponent<Animator>().Play("RobotCageOpen");
         }
+
+        if ((other.gameObject.name == "ObstacleChain") && Input.GetButtonDown("Stay"))
+        {
+            StartCoroutine("LiftDown");
+        }
+    }
+
+    IEnumerator LiftDown()
+    {
+        cageObstacle.GetComponent<Animator>().Play("CageObstacleLift");
+        yield return new WaitForSeconds(20f);
+        cageObstacle.GetComponent<Animator>().Play("CageObstacleLiftDown");
     }
 }
--- Assets/_Scripts/Scripted Events/PulleySystem.cs
+++ Assets/_Scripts/Scripted Events/PulleySystem.cs
@@ -5,58 +5,19 @@ using UnityEngine;
 public class PulleySystem : MonoBehaviour
 {
     public Animator GateAnimator;
-    public float counter;
 
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag =="Companion")
-        {
-            counter = 1;
-        }
 
         if(other.gameObject.tag == "Player")
         {
-            counter = 2;
+            GateAnimator.gameObject.GetComponent<Animator>().Play("GateOpenPuzzle2");
         }
-
-        if(counter == 1)
-        {
-            GateAnimator.GetComponent<Animator>().SetBool("isHalfOpen", true);
-        }
-
-        if (counter == 2)
-        {
-            GateAnimator.GetComponent<Animator>().SetBool("isFullOpen", true);
-        }
-        //print("Contact Count Enter: " + other.contactCount);
-
-        //if(other.gameObject.tag == "Player" || other.gameObject.tag == "Companion")
-        //{
-        //    //open gate halfway
-        //    GateAnimator.GetComponent<Animator>().SetBool("isHalfOpen", true);
-        //}
-        //if (other.gameObject.tag == "Player" && other.gameObject.tag == "Companion")
-        //{
-        //    //open gate fully
-        //    print("GatefullyOpen");
-        //    GateAnimator.GetComponent<Animator>().SetBool("isFullOpen",true);
-        //}
     }
 
     private void OnCollisionExit(Collision other)
     {
-        print("Contact Count Exit: " + other.contactCount);
-        if (other.transform.tag == "Player" || other.transform.tag == "Companion")
-        {
-            print("Come back");
-            //open gate fully
-            GateAnimator.GetComponent<Animator>().SetBool("isHalfOpen",false);
-        }
-        //else if (other.transform.tag == "Player" && other.transform.tag == "Companion")
-        //{
-        //    //open gate full
-        //    GateAnimator.GetComponent<Animator>().SetBool("isFullOpen", false);
-        //}
+
     }
 }
